23a4563e23bed56c1a8f3e89baa4a25df2f8c896
ari <sndaritsune@gmail.com>
2020-11-19 18:19:45 -0300

    initial

---
+++ src/js/data/2020-11-19.js
@@ -0,0 +1,98 @@
+dataSetVersion = '2020-11-19'
+
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = []
+
+dataSet[dataSetVersion].characterData = [
+    {
+        "name": "Koyomi Araragi",
+        "img": "7/70/Koyomi.jpg",
+        "opts": {}
+    },
+    {
+        "name": "Karen Araragi",
+        "img": "4/4f/Karen.jpg",
+        "opts": {}
+    },
+    {
+        "name": "Tsukihi Araragi",
+        "img": "6/69/Tsuhiki.jpg",
+        "opts": {}
+    },
+    {
+        "name": "Hitagi Senjougahara",
+        "img": "d/d4/Senjougahara.jpg",
+        "opts": {}
+    },
+    {
+        "name": "Mayoi Hachikuji",
+        "img": "8/82/Hachikuji.jpg",
+        "opts": {}
+    },
+    {
+        "name": "Suruga Kanbaru",
+        "img": "a/a2/Suruga.jpg",
+        "opts": {}
+    },
+    {
+        "name": "Nadeko Sengoku",
+        "img": "7/79/Nadeko.jpg",
+        "opts": {}
+    },
+    {
+        "name": "Tsubasa Hanekawa",
+        "img": "d/d6/Hanekawa.jpg",
+        "opts": {}
+    },
+    {
+        "name": "Shinobu Oshino",
+        "img": "c/c9/Shinobu.jpg",
+        "opts": {}
+    },
+    {
+        "name": "Sodachi Oikura",
+        "img": "8/8d/Sodachi.jpg",
+        "opts": {}
+    },
+    {
+        "name": "Ougi Oshino",
+        "img": "c/cf/Ougi_icon.jpg",
+        "opts": {}
+    },
+    {
+        "name": "Meme Oshino",
+        "img": "d/d0/Meme.jpg",
+        "opts": {}
+    },
+    {
+        "name": "Deishuu Kaiki",
+        "img": "5/5d/Deishuu.jpg",
+        "opts": {}
+    },
+    {
+        "name": "Yozuru Kagenui",
+        "img": "e/ee/Yozuru_icon.jpg",
+        "opts": {}
+    },
+    {
+        "name": "Yotsugi Ononoki",
+        "img": "8/88/Yotsugi_icon.jpg",
+        "opts": {}
+    },
+    {
+        "name": "Izuko Gaen",
+        "img": "3/3d/Gaen.jpg",
+        "opts": {}
+    },
+    {
+        "name": "Episode",
+        "img": "5/5a/Sode_icon.jpg",
+        "opts": {}
+    },
+    {
+        "name": "Rouka Numachi",
+        "img": "6/6f/Rouka.jpg",
+        "opts": {}
+    }
+]
